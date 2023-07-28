778e5d791d82ff4a206357b324e323d20ebd9dfe
harsh <harshkumar954@gmail.com>
2023-07-28 15:27:20 +0530

    all files addes

---
+++ home/views.py
@@ -0,0 +1,43 @@
+
+from django.shortcuts import render,HttpResponse
+from home.models import Contact
+from datetime import datetime
+from django.contrib import messages
+import json
+import requests
+
+def index(requset):
+  
+
+  return render(requset, 'index.html')
+  
+    #return HttpResponse(" this is HOME page.")
+
+
+       
+
+def about(requset):
+   
+   return render(requset, 'about.html')
+   
+    
+
+def acadmics(requset):
+   return render(requset, 'destination.html')
+     
+
+    # return HttpResponse(" this is your service😉page.")
+
+def contact(requset):
+    if requset.method == "POST":
+        name = requset.POST.get('name') 
+        email = requset.POST.get('email')
+        reason = requset.POST.get('reason')
+        contact = Contact(name=name, email=email, reason=reason, date=datetime.today())
+        contact.save()
+        messages.success(requset, "This is recorded just take care of yourself killer is on the way😈")
+      
+    return render(requset, 'contact.html')
+
+
+ 
